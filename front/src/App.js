78f6c62b04cae8b79e237269aed3666d22d64f43
Ldqtheone <brian.lecarpentier@edu.itescia.fr>
2021-06-15 12:04:07 +0200

    Exercice 1

--- front/src/App.js
+++ front/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   }
 
   callAPI() {
-    fetch("http://localhost:3000/dummy/student")
+    fetch("http://localhost:3000/dummy/students")
       .then(res => res.text())
       .then(res => this.setState({ apiResponse: res }))
       .catch(err => err);
