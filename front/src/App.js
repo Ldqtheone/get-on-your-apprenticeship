edfcadac078440040a5f5b3229f933d68a320ef2
Ldqtheone <brian.lecarpentier@edu.itescia.fr>
2021-06-15 15:45:17 +0200

    add card bootstrap react

--- front/src/App.js
+++ front/src/App.js
@@ -1,60 +1,73 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './hogwarts.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: [] } ;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {apiResponse: []};
+    }
 
-  callAPI() {
-    fetch("http://localhost:3000/dummy/students")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: JSON.parse(res) },()=>console.log(this.state)))
-        .catch(err => err);
-  }
+    callAPI() {
+        fetch("http://localhost:3000/dummy/students")
+            .then(res => res.json())
+            .then(data => this.setState({apiResponse: data}))
+            .catch(err => err);
+    }
 
-  componentWillMount() {
-    this.callAPI();
-  }
+    displayStudent() {
 
-  generateStudentList(){
-      return (
-          this.state.apiResponse.map((listValue, index) => {
-              return (
-                  <tr key={index}>
-                      <td>{listValue.name}</td>
-                      <td>{listValue.house}</td>
-                  </tr>
-              );
-          })
-      )
-  }
+        return this.state.apiResponse.map((student, index) => {
+            return (
+                <Col xs={12} sm={6}>
+                    <Card
+                        bg={"dark"}
+                        key={index}
+                        text={'white'}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Here is a list of all students:
-        </p>
-        <table>
-            <thead>
-               <tr>
-                   <th>Nom</th>
-                   <th>Maison</th>
-               </tr>
-            </thead>
-            <tbody>
-                {this.generateStudentList()}
-            </tbody>
-        </table>
-        </header>
-      </div>
-    );
-  }
+                        className={"mb-2"}
+                    >
+                        <Card.Header>{student.name}</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                House : {student.house}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            );
+        });
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Here is a list of all students:
+                    </p>
+                    <Container>
+                        <Row>
+                            {this.displayStudent()}
+                        </Row>
+                    </Container>
+                </header>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
