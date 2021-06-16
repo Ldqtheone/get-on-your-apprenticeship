37056841b65636bf676220ee902c5e8560cb4b8d
Ldqtheone <brian.lecarpentier@edu.itescia.fr>
2021-06-16 17:53:54 +0200

    Update ApiServices.js

--- front/src/services/ApiServices.js
+++ front/src/services/ApiServices.js
@@ -7,6 +7,7 @@ export function getAllStudents() {
         .catch(err => err);
 }
 
+
 export function getAllStudentsByHouse(house) {
     return fetch("http://localhost:3000/real/students?house="+house)
         .then(res => res.json())
